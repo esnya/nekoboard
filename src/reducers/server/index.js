1296684b459fe49e2ec8a680e079e660151d34fe
ukatama <dev.ukatama@gmail.com>
2016-02-22 06:25:41 +0900

    Remove unused server side redux codes

--- src/reducers/server/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { boards } from './board';
-import { shapes } from './shapes';
-
-export const reducer = combineReducers({
-    boards,
-    shapes,
-});
--- src/stores/server.js
+++
@@ -1,5 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-import { reducer } from '../reducers/server';
-import { middleware } from '../middlewares/server';
-
-export const store = applyMiddleware(...middleware)(createStore)(reducer);
