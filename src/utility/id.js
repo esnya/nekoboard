5ff7135a5348bbb3fa5b6555a8a08d1fcd873e4a
ukatama <dev.ukatama@gmail.com>
2016-03-01 13:03:31 +0900

    Fix test of utility/id

--- src/utility/id.js
+++ src/utility/id.js
@@ -3,5 +3,15 @@ import { createHash } from 'crypto';
 const seed = Date.now() * Math.random();
 let serial = 1;
 
-export const generate = (len, data = `${seed}:${serial++}`) =>
-    createHash('sha1').update(data).digest('hex').substr(0, len);
+/**
+ * Generate unique ID
+ * @param{number} len - Length of ID
+ * @param{data} data - Unique value to generate ID
+ * @returns{string} Unique ID
+ */
+export function generate(len, data = `${seed}:${serial++}`) {
+    return createHash('sha1')
+        .update(data)
+        .digest('hex')
+        .substr(0, len);
+}
